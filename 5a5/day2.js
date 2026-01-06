b3108e517131a4901c8ffdbc8ed1b661a6d88b5a
srujana1801 <239y1a05a5@ksrmce.ac.in>
2026-01-06 12:10:52 +0530

    Add files via upload

---
+++ 5a5/day2.js
@@ -0,0 +1,14 @@
+//functions:a block of code designed to perform a particular
+var a=10;
+var b=20;
+function day2(){
+    let sum=a+b;
+    console.log("the sum is"+sum);
+    document.writeln(sum)
+}
+function day3(){
+    
+}
+function day4(){
+    
+}
